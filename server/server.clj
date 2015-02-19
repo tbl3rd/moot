8569989f90b1967460bd749e863f91f44c4c6374
Tom Lyons <tbl3rd@gmail.com>
2015-02-19 13:54:04 -0500

    Inject some logging, and so on.  Not sure why only the "deprecated" wrap-file-info works and not the newer middleware.

--- server/server.clj
+++ server/server.clj
@@ -1,16 +1,28 @@
 (ns server
   (:require [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.file-info :refer [wrap-file-info]]))
+            [ring.middleware.file :refer [wrap-file]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.not-modified :refer [wrap-not-modified]]))
+
+(defn wrap-println
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (println [:request request])
+      (println [:response response])
+      response)))
 
 (defn handler
   [request]
-  (println [:server/handler {:request request}])
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "Hello from Moot!"})
+  (println [:server/handler {:request request}]))
 
 (def moot-app
   "The server callback entry point."
   (-> handler
-      (wrap-resource "target")
-      (wrap-file-info)))
+      ;; (wrap-resource "target")
+      (wrap-file "target" {:index-files? true})
+      wrap-file-info                    ; works!
+      ;; wrap-content-type                 ; does not work
+      ;; wrap-not-modified                 ; does not work
+      wrap-println))
